baa309213a9998c903aceea9911b796f5f40d8b2
ddangju <duswn4338@gmail.com>
2022-03-02 11:42:59 +0900

    add: 주문수량 수정 가능(action)to product.js

--- src/basket/product.js
+++ src/basket/product.js
@@ -10,19 +10,48 @@ const Product = (props) => {
     navigate("/basket");
   };
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    ///클릭해서 들어온 버튼의 수량을 플러스
+    console.log(e);
     setPrice((prev) => prev + 1);
   };
   return (
     <>
       <h1>상품</h1>
-      <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="shoes" />
-      <div>
-        <button onClick={handleClick}>+</button>
-        <button>-</button>
-      </div>
-      <h2>{props.state[0].quan}</h2>
-      <button onClick={goBasket}>카트바로가기</button>
+      {props.state.map((item, idx) => {
+        return (
+          <div key={idx}>
+            <img
+              src={`https://codingapple1.github.io/shop/shoes${idx + 1}.jpg`}
+              alt="shoes"
+            />
+            <div>
+              {/* <button className={`button${idx + 1}`} onClick={handleClick}>
+                +
+              </button> */}
+              <button
+                onClick={() => {
+                  props.dispatch({
+                    type: { increment: "increment", id: item.id },
+                  });
+                }}
+              >
+                +
+              </button>
+              <button
+                onClick={() => {
+                  props.dispatch({ type: "decrement" });
+                }}
+              >
+                -
+              </button>
+            </div>
+            <h1>{item.name}</h1>
+            <h2>{item.quan}</h2>
+            <button onClick={goBasket}>카트바로가기</button>
+          </div>
+        );
+      })}
     </>
   );
 };
@@ -31,7 +60,7 @@ const Product = (props) => {
 //object를 리턴한다.
 
 function state를props화(state) {
-  console.log(state, "<<<State");
+  // console.log(state, "<<<State");
 
   return {
     state: state,
